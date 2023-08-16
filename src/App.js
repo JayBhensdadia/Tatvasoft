d7f05b07a73ebc229f4f1d8113c78062793460e0
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-08-16 13:58:15 +0530

    day 2 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import BooksList from './components/BooksList';
+import HomePage from './components/HomePage';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import PageNotFound from './components/PageNotFound';
+
 
 function App() {
+
+  const name = "Jay";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          just created react app!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      
+      
+      
+
+
+      <BrowserRouter>
+
+        <NavLink style={{marginRight: 10}} to="/">Home</NavLink>
+        <NavLink to="/books">Books</NavLink>
+
+        <Routes>
+          <Route path='/' element={<HomePage userName={name} />}></Route>
+          <Route path='/books' element={<BooksList />}></Route>
+          <Route path='*' element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+
+
     </div>
   );
 }
---
+++ src/components/BooksList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BooksList = () => {
+    return (
+        <>
+            <h1>BooksList Page</h1>
+        </>
+    )
+};
+
+export default BooksList;
